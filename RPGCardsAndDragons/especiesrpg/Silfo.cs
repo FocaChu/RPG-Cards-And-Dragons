3a6591f63430be5633cc4c5085fdddb49182f084
FocaChu <joaoguttocordeiro@gmail.com>
2025-05-02 22:32:34 -0300

    Nova especie Silfo

---
+++ RPGCardsAndDragons/especiesrpg/Silfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CardsAndDragonsJogo;
+
+namespace CardsAndDragons.ClassesEspecies
+{
+    public class Silfo : EspecieRPG
+    {
+        public override string NomeEspecie => "Silfo";
+
+        public override string DescricaoEspecie => "Sedutor e voraz. Ladrão de sangue.";
+    }
+}
